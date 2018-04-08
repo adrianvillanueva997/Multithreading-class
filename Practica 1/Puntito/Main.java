b7f1681e8c608e4616afe521f10192da659e6e0e
Adrián Villanueva <thexiao77@users.noreply.github.com>
2018-04-08 20:56:00 +0200

    Update Main.java

--- Practica 1/Puntito/Main.java
+++ Practica 1/Puntito/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Hilo hilo = new Hilo(".", 1000);
+        Hilo hilo = new Hilo(".", 100);
         hilo.start();
     }
 }
